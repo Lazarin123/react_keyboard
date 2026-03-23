5bdf1bb32853a4617e167a3eecfa1902eb6b2703
Samuel Lazarin <samuel.lazarin12@gmail.com>
2026-03-23 16:03:57 -0300

    add project complete

--- src/App.tsx
+++ src/App.tsx
@@ -32,9 +32,7 @@ export class App extends React.Component<{}, State> {
     return (
       <div className="App">
         {pressedKey ? (
-          <p className="App__message">
-            The last pressed key is [{pressedKey}]
-          </p>
+          <p className="App__message">The last pressed key is [{pressedKey}]</p>
         ) : (
           <p className="App__message">Nothing was pressed yet</p>
         )}
